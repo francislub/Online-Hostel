713518609c41da3d85e620d7e30dcafa756a6e95
francislub <lubanjwafrancis@gmail.com>
2024-04-03 22:42:40 +0300

    initail comments

---
+++ footer.php
@@ -0,0 +1,9 @@
+
+<div class="foot">
+
+<p> <a class="pull-right" href="https://www.facebook.com/Raselhossain120"target="_blank">H.E LEONE CHIRODZA</a></br>
+<a class="pull-right" href="https://www.facebook.com/hossain22215"target="_blank">BUSA PRESIDENT </a></p>
+
+<style> .foot{text-align:center; border: 2px solid black;}</style>
+
+</div>
